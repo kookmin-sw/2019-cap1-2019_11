1ee246e68bb35157f2181d95748e44c554e526a9
Uk-yonge <1a1a11a@naver.com>
2019-04-10 00:24:59 +0900

    Add files via upload

---
+++ yoloface/v2/utils.py
@@ -0,0 +1,342 @@
+# *******************************************************************
+#
+# Author : Thanh Nguyen, 2018
+# Email  : sthanhng@gmail.com
+# Github : https://github.com/sthanhng
+#
+# BAP, AI Team
+# Face detection using the YOLOv3 algorithm
+#
+# Description : utils.py
+# This file contains the code of the parameters and help functions
+#
+# *******************************************************************
+
+
+import datetime
+import numpy as np
+import cv2
+
+import argparse
+import sys
+import os
+import time
+import face_recognition
+import camera
+
+# -------------------------------------------------------------------
+# Parameters
+# -------------------------------------------------------------------
+
+CONF_THRESHOLD = 0.5
+NMS_THRESHOLD = 0.4
+IMG_WIDTH = 416
+IMG_HEIGHT = 416
+
+# Default colors
+COLOR_BLUE = (255, 0, 0)
+COLOR_GREEN = (0, 255, 0)
+COLOR_RED = (0, 0, 255)
+COLOR_WHITE = (255, 255, 255)
+COLOR_YELLOW = (0, 255, 255)
+
+
+###########
+parser = argparse.ArgumentParser()
+parser.add_argument('--model-cfg', type=str, default='./cfg/yolov3-face.cfg',
+                    help='path to config file')
+parser.add_argument('--model-weights', type=str,
+                    default='./model-weights/yolov3-wider_16000.weights',
+                    help='path to weights of model')
+parser.add_argument('--image', type=str, default='',
+                    help='path to image file')
+parser.add_argument('--video', type=str, default='',
+                    help='path to video file')
+parser.add_argument('--src', type=int, default=0,
+                    help='source of the camera')
+parser.add_argument('--output-dir', type=str, default='outputs/',
+                    help='path to the output directory')
+args = parser.parse_args()
+
+###########
+
+
+
+
+net = cv2.dnn.readNetFromDarknet(args.model_cfg, args.model_weights)
+net.setPreferableBackend(cv2.dnn.DNN_BACKEND_OPENCV)
+net.setPreferableTarget(cv2.dnn.DNN_TARGET_CPU)
+
+# -------------------------------------------------------------------
+# Help functions
+# -------------------------------------------------------------------
+
+# Get the names of the output layers
+def get_outputs_names(net):
+    # Get the names of all the layers in the network
+    layers_names = net.getLayerNames()
+
+    # Get the names of the output layers, i.e. the layers with unconnected
+    # outputs
+    return [layers_names[i[0] - 1] for i in net.getUnconnectedOutLayers()]
+
+
+# Draw the predicted bounding box
+def draw_predict(frame, conf, left, top, right, bottom):
+    # Draw a bounding box.
+    cv2.rectangle(frame, (left, top), (right, bottom), COLOR_YELLOW, 2)
+
+    text = '{:.2f}'.format(conf)
+
+    # Display the label at the top of the bounding box
+    label_size, base_line = cv2.getTextSize(text, cv2.FONT_HERSHEY_SIMPLEX, 0.5, 1)
+
+    top = max(top, label_size[1])
+    cv2.putText(frame, text, (left, top - 4), cv2.FONT_HERSHEY_SIMPLEX, 0.4,
+                COLOR_WHITE, 1)
+
+
+def post_process(frame, outs, conf_threshold, nms_threshold):
+    frame_height = frame.shape[0]
+    frame_width = frame.shape[1]
+
+    # Scan through all the bounding boxes output from the network and keep only
+    # the ones with high confidence scores. Assign the box's class label as the
+    # class with the highest score.
+    confidences = []
+    boxes = []
+    final_boxes = []
+    for out in outs:
+        for detection in out:
+            scores = detection[5:]
+            class_id = np.argmax(scores)
+            confidence = scores[class_id]
+            if confidence > conf_threshold:
+                center_x = int(detection[0] * frame_width)
+                center_y = int(detection[1] * frame_height)
+                width = int(detection[2] * frame_width)
+                height = int(detection[3] * frame_height)
+                left = int(center_x - width / 2)
+                top = int(center_y - height / 2)
+                confidences.append(float(confidence))
+                boxes.append([left, top, width, height])
+
+    # Perform non maximum suppression to eliminate redundant
+    # overlapping boxes with lower confidences.
+    indices = cv2.dnn.NMSBoxes(boxes, confidences, conf_threshold,
+                               nms_threshold)
+
+    for i in indices:
+        i = i[0]
+        box = boxes[i]
+        left = box[0]
+        top = box[1]
+        width = box[2]
+        height = box[3]
+        final_boxes.append(box)
+        left, top, right, bottom = refined_box(left, top, width, height)
+        # draw_predict(frame, confidences[i], left, top, left + width,
+        #              top + height)
+#draw_predict(frame, confidences[i], left, top, right, bottom)
+    return final_boxes
+
+def post_process2(frame, outs, conf_threshold, nms_threshold):
+    frame_height = frame.shape[0]
+    frame_width = frame.shape[1]
+    
+    # Scan through all the bounding boxes output from the network and keep only
+    # the ones with high confidence scores. Assign the box's class label as the
+    # class with the highest score.
+    confidences = []
+    boxes = []
+    final_boxes = []
+    for out in outs:
+        for detection in out:
+            scores = detection[5:]
+            class_id = np.argmax(scores)
+            confidence = scores[class_id]
+            if confidence > conf_threshold:
+                center_x = int(detection[0] * frame_width)
+                center_y = int(detection[1] * frame_height)
+                width = int(detection[2] * frame_width)
+                height = int(detection[3] * frame_height)
+                left = int(center_x - width / 2)
+                top = int(center_y - height / 2)
+                confidences.append(float(confidence))
+                boxes.append([left, top, width, height])
+
+    # Perform non maximum suppression to eliminate redundant
+    # overlapping boxes with lower confidences.
+    indices = cv2.dnn.NMSBoxes(boxes, confidences, conf_threshold,
+                               nms_threshold)
+
+    for i in indices:
+        i = i[0]
+        box = boxes[i]
+        left = box[0]
+        top = box[1]
+        right = box[2] + left
+        bottom = box[3] + top
+        final_boxes.append([top, right, bottom, left])
+    return final_boxes
+
+class FPS:
+    def __init__(self):
+        # store the start time, end time, and total number of frames
+        # that were examined between the start and end intervals
+        self._start = None
+        self._end = None
+        self._num_frames = 0
+
+    def start(self):
+        self._start = datetime.datetime.now()
+        return self
+
+    def stop(self):
+        self._end = datetime.datetime.now()
+
+    def update(self):
+        # increment the total number of frames examined during the
+        # start and end intervals
+        self._num_frames += 1
+
+    def elapsed(self):
+        # return the total number of seconds between the start and
+        # end interval
+        return (self._end - self._start).total_seconds()
+
+    def fps(self):
+        # compute the (approximate) frames per second
+        return self._num_frames / self.elapsed()
+
+def refined_box(left, top, width, height):
+    right = left + width
+    bottom = top + height
+
+    original_vert_height = bottom - top
+    top = int(top + original_vert_height * 0.15)
+    bottom = int(bottom - original_vert_height * 0.05)
+
+    margin = ((bottom - top) - (right - left)) // 2
+    left = left - margin if (bottom - top - right + left) % 2 == 0 else left - margin - 1
+
+    right = right + margin
+
+    return left, top, right, bottom
+
+
+
+class FaceRecog():
+    def __init__(self):
+        # Using OpenCV to capture from device 0. If you have trouble capturing
+        # from a webcam, comment the line below out and use a video file
+        # instead.
+        print('init')
+        self.camera = camera.VideoCamera()
+        
+        self.known_face_encodings = []
+        self.known_face_names = []
+        
+        # Load sample pictures and learn how to recognize it.
+        dirname = 'knowns'
+        files = os.listdir(dirname)
+        for filename in files:
+            name, ext = os.path.splitext(filename)
+            if ext == '.jpg':
+                self.known_face_names.append(name)
+                pathname = os.path.join(dirname, filename)
+                img = face_recognition.load_image_file(pathname)
+                face_encoding = face_recognition.face_encodings(img)[0]
+                self.known_face_encodings.append(face_encoding)
+        
+        # Initialize some variables
+        self.face_locations = []
+        self.face_encodings = []
+        self.face_names = []
+        self.process_this_frame = True
+
+    def __del__(self):
+        del self.camera
+
+    def get_frame(self):
+        # Grab a single frame of video
+        frame = self.camera.get_frame()
+        #print(frame)
+        # Create a 4D blob from a frame.
+        blob = cv2.dnn.blobFromImage(frame, 1 / 255, (IMG_WIDTH, IMG_HEIGHT),
+                                     [0, 0, 0], 1, crop=False)
+        # Sets the input to the network
+        net.setInput(blob)
+        
+        
+        outs = net.forward(get_outputs_names(net))
+        
+        # Remove the bounding boxes with low confidence
+        #[top, right, bottom, left])
+        #faces = post_process2(frame, outs, CONF_THRESHOLD, NMS_THRESHOLD)
+        #print(faces)
+        
+        # Resize frame of video to 1/4 size for faster face recognition processing
+        small_frame = cv2.resize(frame, (0, 0), fx=0.25, fy=0.25)
+        #small_faces = cv2.resize(faces, (0, 0), fx=0.25, fy=0.25)
+
+        # Convert the image from BGR color (which OpenCV uses) to RGB color (which face_recognition uses)
+        rgb_small_frame = small_frame[:, :, ::-1]
+        #rgb_small_faces = small_faces[:, :, ::-1]
+        faces = post_process2(rgb_small_frame, outs, CONF_THRESHOLD, NMS_THRESHOLD)
+
+        # Only process every other frame of video to save time
+        if self.process_this_frame:
+            print('process start')
+            # Find all the faces and face encodings in the current frame of video
+#            self.face_locations = face_recognition.face_locations(rgb_small_frame)
+#self.face_locations = faces
+            print(faces)
+            self.face_encodings = face_recognition.face_encodings(rgb_small_frame, faces)
+#self.face_encodings = face_recognition.face_encodings(frame, faces)
+
+            self.face_names = []
+            for face_encoding in self.face_encodings:
+                # See if the face is a match for the known face(s)
+                distances = face_recognition.face_distance(self.known_face_encodings, face_encoding)
+                min_value = min(distances)
+                
+                # tolerance: How much distance between faces to consider it a match. Lower is more strict.
+                # 0.6 is typical best performance.
+                name = "Unknown"
+                if min_value < 0.45:
+                    index = np.argmin(distances)
+                    name = self.known_face_names[index]
+                
+                self.face_names.append(name)
+        
+        self.process_this_frame = not self.process_this_frame
+        
+        # Display the results
+        # return => left top right bottom
+        
+        for (top, right, bottom, left), name in zip(faces, self.face_names):
+            # Scale back up face locations since the frame we detected in was scaled to 1/4 size
+            top *= 4
+            right *= 4
+            bottom *= 4
+            left *= 4
+
+            # Draw a box around the face
+            cv2.rectangle(frame, (left, top), (right, bottom+10), (0, 0, 255), 2)
+            
+            # Draw a label with a name below the face
+            #cv2.rectangle(frame, (left, bottom - 35), (right, bottom), (0, 0, 255), cv2.FILLED)
+            font = cv2.FONT_HERSHEY_DUPLEX
+            cv2.putText(frame, name, (left + 6, bottom - 6), font, 1.0, (255, 255, 255), 1)
+        
+        return frame
+
+    def get_jpg_bytes(self):
+        frame = self.get_frame()
+        # We are using Motion JPEG, but OpenCV defaults to capture raw images,
+        # so we must encode it into JPEG in order to correctly display the
+        # video stream.
+        ret, jpg = cv2.imencode('.jpg', frame)
+        return jpg.tobytes()
+
