8fc6a1ea2aa21d189c94e40ec50466e9e6b734b2
Uk-yonge <1a1a11a@naver.com>
2019-04-10 23:27:44 +0900

    add cam version and video version

---
+++ yoloface/v2/yoloface_video.py
@@ -0,0 +1,99 @@
+
+import argparse
+import sys
+import os
+
+import datetime
+import time
+import face_recognition
+import cv2
+import camera
+import numpy as np
+
+from utils import *
+
+#####################################################################
+parser = argparse.ArgumentParser()
+parser.add_argument('--model-cfg', type=str, default='./cfg/yolov3-face.cfg',
+                    help='path to config file')
+parser.add_argument('--model-weights', type=str,
+                    default='./model-weights/yolov3-wider_16000.weights',
+                    help='path to weights of model')
+parser.add_argument('--image', type=str, default='',
+                    help='path to image file')
+parser.add_argument('--video', type=str, default='',
+                    help='path to video file')
+parser.add_argument('--src', type=int, default=0,
+                    help='source of the camera')
+parser.add_argument('--output-dir', type=str, default='outputs/',
+                    help='path to the output directory')
+args = parser.parse_args()
+
+
+# check outputs directory
+if not os.path.exists(args.output_dir):
+    print('==> Creating the {} directory...'.format(args.output_dir))
+    os.makedirs(args.output_dir)
+else:
+    print('==> Skipping create the {} directory...'.format(args.output_dir))
+
+# Give the configuration and weight files for the model and load the network
+# using them.
+net = cv2.dnn.readNetFromDarknet(args.model_cfg, args.model_weights)
+net.setPreferableBackend(cv2.dnn.DNN_BACKEND_OPENCV)
+net.setPreferableTarget(cv2.dnn.DNN_TARGET_CPU)
+
+
+def _main():
+############# CAM ##################
+
+    output_file=''
+
+    if args.video:
+        if not os.path.isfile(args.video):
+            print("[!] ==> Input video file {} doesn't exist".format(args.video))
+            sys.exit(1)
+        cap = cv2.VideoCapture(args.video)
+        output_file = args.video[:-4].rsplit('/')[-1] + '_yoloface.avi'
+
+    # Get the video writer initialized to save the output video
+    if not args.image:
+        video_writer = cv2.VideoWriter(os.path.join(args.output_dir, output_file),
+                                       cv2.VideoWriter_fourcc('M', 'J', 'P', 'G'),
+                                       cap.get(cv2.CAP_PROP_FPS), (
+                                           round(cap.get(cv2.CAP_PROP_FRAME_WIDTH)),
+                                           round(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))))
+
+
+    face_recog=FaceRecog_video()
+    while True:
+        has_frame, frame=cap.read()
+
+
+        if not has_frame:
+            print('[i] ==> Done processing!!!')
+            print('[i] ==> Output file is stored at', os.path.join(args.output_dir, output_file))
+            cv2.waitKey(1000)
+            break
+
+        frame=face_recog.get_frame(frame)
+    # Save the output video to file
+        if args.video:
+            # cv2.imwrite(os.path.join(args.output_dir, output_file), frame.astype(np.uint8))
+            video_writer.write(frame.astype(np.uint8))
+
+        key = cv2.waitKey(1)
+        if key == 27 or key == ord('q'):
+            print('[i] ==> Interrupted by user!')
+            break
+
+    # do a bit of cleanup
+    cap.release()
+    cv2.destroyAllWindows()
+    print('finish')
+
+
+
+
+if __name__ == '__main__':
+    _main()
