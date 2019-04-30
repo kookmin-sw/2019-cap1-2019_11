6e9f62389bc71c79dd8425e2f063cf289283315c
Uk-yonge <1a1a11a@naver.com>
2019-04-30 23:20:38 +0900

    영상에서 음성 추출 및 블러 처리된 파일과 Merge 기능 추가

--- yoloface/v2/yoloface_gpu.py
+++ yoloface/v2/yoloface_gpu.py
@@ -6,8 +6,79 @@ import face_recognition
 import os
 import datetime
 import time
+import sys
+import subprocess as sp
+
+from moviepy.tools import subprocess_call
+from moviepy.config import get_setting
 
 #####################################################################
+
+
+
+
+def ffmpeg_movie_from_frames(filename, folder, fps, digits=6):
+    """
+    Writes a movie out of the frames (picture files) in a folder.
+    Almost deprecated.
+    """
+    s = "%" + "%02d" % digits + "d.png"
+    cmd = [get_setting("FFMPEG_BINARY"), "-y", "-f","image2",
+             "-r", "%d"%fps,
+             "-i", os.path.join(folder,folder) + '/' + s,
+             "-b", "%dk"%bitrate,
+             "-r", "%d"%self.fps,
+             filename]
+
+    subprocess_call(cmd)
+
+
+
+def ffmpeg_extract_subclip(filename, t1, t2, targetname=None):
+    """ makes a new video file playing video file ``filename`` between
+        the times ``t1`` and ``t2``. """
+    name,ext = os.path.splitext(filename)
+    if not targetname:
+        T1, T2 = [int(1000*t) for t in [t1, t2]]
+        targetname = name+ "%sSUB%d_%d.%s"(name, T1, T2, ext)
+
+    cmd = [get_setting("FFMPEG_BINARY"),"-y",
+      "-i", filename,
+      "-ss", "%0.2f"%t1,
+      "-t", "%0.2f"%(t2-t1),
+      "-vcodec", "copy", "-acodec", "copy", targetname]
+
+    subprocess_call(cmd)
+
+
+
+def ffmpeg_merge_video_audio(video,audio,output, vcodec='copy',acodec='copy', ffmpeg_output=False,verbose = True):
+    """ merges video file ``video`` and audio file ``audio`` into one
+        movie file ``output``. """
+    cmd = [get_setting("FFMPEG_BINARY"), "-y", "-i", audio,"-i", video,
+             "-vcodec", vcodec, "-acodec", acodec, output]
+
+    subprocess_call(cmd)
+
+
+
+def ffmpeg_extract_audio(inputfile,output,bitrate=3000,fps=44100):
+    """ extract the sound from a video file and save it in ``output`` """
+    cmd = [get_setting("FFMPEG_BINARY"), "-y", "-i", inputfile, "-ab", "%dk"%bitrate,
+         "-ar", "%d"%fps, output]
+    subprocess_call(cmd)
+
+
+
+def ffmpeg_resize(video,output,size):
+    """ resizes ``video`` to new size ``size`` and write the result
+        in file ``output``. """
+    cmd= [get_setting("FFMPEG_BINARY"), "-i", video, "-vf", "scale=%d:%d"%(res[0], res[1]),
+             output]
+
+    subprocess_call(cmd)
+
+
 def get_args():
     parser = argparse.ArgumentParser()
     parser.add_argument('--model', type=str, default='model-weights/YOLO_Face.h5',
@@ -50,6 +121,20 @@ def _main():
     end = datetime.datetime.now()
     print(now)
     print(end)
+
+
+    # 음성 추출 extract the sound from a video file and save it in output
+
+    inputfile=args.video
+    output='outputs/outAudio.mp3'
+    ffmpeg_extract_audio(inputfile, output, bitrate=3000, fps=44100)
+
+    audio='outputs/outAudio.mp3'
+    video='outputs/output_video.avi'
+    outputfinal='outputs/finalvideo.mp4'
+    # merges video file video and audio file audio into one movie file output.
+
+    ffmpeg_merge_video_audio(video, audio, outputfinal, vcodec='copy', acodec='copy', ffmpeg_output=False, verbose=True)
 
 if __name__ == "__main__":
     _main()
--- yoloface/v2/yoloface_video.py
+++ yoloface/v2/yoloface_video.py
@@ -1,4 +1,4 @@
-
+# -*- coding: utf-8 -*-
 import argparse
 import sys
 import os
@@ -11,6 +11,81 @@ import camera
 import numpy as np
 
 from utils import *
+
+
+import sys
+import subprocess as sp
+
+from moviepy.tools import subprocess_call
+from moviepy.config import get_setting
+
+#####################################################################
+
+
+
+
+def ffmpeg_movie_from_frames(filename, folder, fps, digits=6):
+    """
+    Writes a movie out of the frames (picture files) in a folder.
+    Almost deprecated.
+    """
+    s = "%" + "%02d" % digits + "d.png"
+    cmd = [get_setting("FFMPEG_BINARY"), "-y", "-f","image2",
+             "-r", "%d"%fps,
+             "-i", os.path.join(folder,folder) + '/' + s,
+             "-b", "%dk"%bitrate,
+             "-r", "%d"%self.fps,
+             filename]
+
+    subprocess_call(cmd)
+
+
+
+def ffmpeg_extract_subclip(filename, t1, t2, targetname=None):
+    """ makes a new video file playing video file ``filename`` between
+        the times ``t1`` and ``t2``. """
+    name,ext = os.path.splitext(filename)
+    if not targetname:
+        T1, T2 = [int(1000*t) for t in [t1, t2]]
+        targetname = name+ "%sSUB%d_%d.%s"(name, T1, T2, ext)
+
+    cmd = [get_setting("FFMPEG_BINARY"),"-y",
+      "-i", filename,
+      "-ss", "%0.2f"%t1,
+      "-t", "%0.2f"%(t2-t1),
+      "-vcodec", "copy", "-acodec", "copy", targetname]
+
+    subprocess_call(cmd)
+
+
+
+def ffmpeg_merge_video_audio(video,audio,output, vcodec='copy',acodec='copy', ffmpeg_output=False,verbose = True):
+    """ merges video file ``video`` and audio file ``audio`` into one
+        movie file ``output``. """
+    cmd = [get_setting("FFMPEG_BINARY"), "-y", "-i", audio,"-i", video,
+             "-vcodec", vcodec, "-acodec", acodec, output]
+
+    subprocess_call(cmd)
+
+
+
+def ffmpeg_extract_audio(inputfile,output,bitrate=3000,fps=44100):
+    """ extract the sound from a video file and save it in ``output`` """
+    cmd = [get_setting("FFMPEG_BINARY"), "-y", "-i", inputfile, "-ab", "%dk"%bitrate,
+         "-ar", "%d"%fps, output]
+    subprocess_call(cmd)
+
+
+
+def ffmpeg_resize(video,output,size):
+    """ resizes ``video`` to new size ``size`` and write the result
+        in file ``output``. """
+    cmd= [get_setting("FFMPEG_BINARY"), "-i", video, "-vf", "scale=%d:%d"%(res[0], res[1]),
+             output]
+
+    subprocess_call(cmd)
+
+
 
 #####################################################################
 parser = argparse.ArgumentParser()
@@ -64,7 +139,6 @@ def _main():
                                            round(cap.get(cv2.CAP_PROP_FRAME_WIDTH)),
                                            round(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))))
 
-
     face_recog=FaceRecog_video()
     while True:
         has_frame, frame=cap.read()
@@ -91,6 +165,20 @@ def _main():
     cap.release()
     cv2.destroyAllWindows()
     print('finish')
+    # 음성 추출 extract the sound from a video file and save it in output
+
+    inputfile=args.video
+    output='outputs/outAudio.mp3'
+    ffmpeg_extract_audio(inputfile, output, bitrate=3000, fps=44100)
+
+    audio='outputs/outAudio.mp3'
+    video=args.output_dir+'/'+output_file
+    outputfinal='outputs/finalvideo.mp4'
+
+    # merges video file video and audio file audio into one movie file output.
+
+    ffmpeg_merge_video_audio(video, audio, outputfinal, vcodec='copy', acodec='copy', ffmpeg_output=False, verbose=True)
+
 
 
 
