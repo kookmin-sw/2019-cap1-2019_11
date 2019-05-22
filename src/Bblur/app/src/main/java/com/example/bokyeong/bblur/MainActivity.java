95dcf72c4f980fdae243716a0134fe50b7e566a9
KwonBoKyeung <kbg514@naver.com>
2019-05-22 12:55:39 +0900

    Bblur 메인 화면 구현

--- src/Bblur/app/src/main/java/com/example/bokyeong/bblur/MainActivity.java
+++ src/Bblur/app/src/main/java/com/example/bokyeong/bblur/MainActivity.java
@@ -1,98 +1,56 @@
 package com.example.bokyeong.bblur;
 
 import android.content.Intent;
-import android.database.Cursor;
-import android.graphics.BitmapFactory;
-import android.net.Uri;
-import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Toast;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements  View.OnClickListener{
 
-    private static int PICK_IMAGE_REQUEST = 1;
-    private static int PICK_VIDEO_REQUEST = 2;
+    Button buttonMyUpload;
+    Button buttonPhotoUpload;
+    Button buttonFaceUpload;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        buttonMyUpload = (Button) findViewById(R.id.buttonMyUpload);
+        buttonPhotoUpload = (Button) findViewById(R.id.buttonPhotoUpload);
+        buttonFaceUpload = (Button) findViewById(R.id.buttonFaceUpload);
+
+        buttonMyUpload.setOnClickListener(this);
+        buttonPhotoUpload.setOnClickListener(this);
+        buttonFaceUpload.setOnClickListener(this);
     }
 
-    // 갤러리에서 이미지 선택
-    public void selectPhotofromGallery(View view) {
-
-        startActivityForResult(
-            Intent.createChooser(
-                new Intent(Intent.ACTION_GET_CONTENT)
-                        .setType("image/*"), "Select Picture"), PICK_IMAGE_REQUEST);
+    private void myUpload() {
+        Intent intent = new Intent(getApplicationContext(), MyUploadActivity.class);
+        startActivity(intent);
     }
 
-    // 갤러리에서 동영상 선택
-    public void selectVideofromGallery(View view) {
-
-        startActivityForResult(
-                Intent.createChooser(
-                        new Intent(Intent.ACTION_GET_CONTENT)
-                                .setType("video/*"), "Select Video"), PICK_VIDEO_REQUEST);
+    private void photoUpload() {
+        Intent intent = new Intent(getApplicationContext(), PhotoUploadActivity.class);
+        startActivity(intent);
     }
 
+    private void faceUpload() {
+        Intent intent = new Intent(getApplicationContext(), FaceUploadActivity.class);
+        startActivity(intent);
+    }
 
-    // image path 얻기
-    public String getPath(Uri uri) {
-
-        if( uri == null ) {
-            return null;
+    @Override
+    public void onClick(View v) {
+        if(v == buttonMyUpload) {
+            myUpload();
         }
-
-        // this will only work for images selected from gallery
-        String[] projection = { MediaStore.Images.Media.DATA };
-        Cursor cursor = managedQuery(uri, projection, null, null, null);
-        if( cursor != null ){
-            int column_index = cursor
-                    .getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
-            cursor.moveToFirst();
-            return cursor.getString(column_index);
+        if(v == buttonPhotoUpload) {
+            photoUpload();
         }
-
-        return uri.getPath();
-    }
-
-    // 데이터 파일 선택 후의 결과 처리
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        try {
-            //이미지를 선택한 경우
-            if (requestCode == PICK_IMAGE_REQUEST && resultCode == RESULT_OK && null != data) {
-
-                Uri uri = (Uri)data.getData();
-
-                String selectedImagePath = getPath(uri);
-
-                Intent intent = new Intent(MainActivity.this, SendToServer.class);
-                intent.putExtra("imageUri", uri);
-                intent.putExtra("imagePath", selectedImagePath);
-
-
-                startActivity(intent);
-
-            }
-            //비디오를 선택한 경우
-            else if (requestCode == PICK_VIDEO_REQUEST && resultCode == RESULT_OK && null != data)
-            {
-                Uri uri = (Uri)data.getData();
-
-                Intent intent = new Intent(MainActivity.this, VideoActivity.class);
-                intent.putExtra("videoUri", uri);
-
-                startActivity(intent);
-            }
-        }catch (Exception e) {
-            Toast.makeText(this, "오류가 발생했습니다.", Toast.LENGTH_LONG).show();
-            e.printStackTrace();
+        if(v == buttonFaceUpload) {
+            faceUpload();
         }
     }
 }
