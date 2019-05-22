520ea81d884f2fe8d160cb13e4e26b262271bbf7
KwonBoKyeung <kbg514@naver.com>
2019-05-22 13:48:17 +0900

    데이터 전송파일

---
+++ src/php_server/upload.php
@@ -0,0 +1,18 @@
+<?php
+
+        if($_SERVER['REQUEST_METHOD']=='POST'){
+                $file_name = $_FILES['myFile']['name'];
+                $file_size = $_FILES['myFile']['size'];
+                $file_type = $_FILES['myFile']['type'];
+                $temp_name = $_FILES['myFile']['tmp_name'];
+
+                $location = "uploads/";
+
+                move_uploaded_file($temp_name, $location.$file_name);
+                echo "/uploads/".$file_name;
+        }else{
+                echo "Error";
+        }
+
+        echo " check this : ";
+        print_r($_FILES); ?>
