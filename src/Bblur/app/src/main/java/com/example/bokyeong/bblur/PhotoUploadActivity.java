b98c0dd97c55de5b9b3d61c73124ee13bd654454
KwonBoKyeung <kbg514@naver.com>
2019-05-27 19:46:47 +0900

    파일 다운로드 및 토스트 추가

--- src/Bblur/app/src/main/java/com/example/bokyeong/bblur/PhotoUploadActivity.java
+++ src/Bblur/app/src/main/java/com/example/bokyeong/bblur/PhotoUploadActivity.java
@@ -1,6 +1,7 @@
 package com.example.bokyeong.bblur;
 
 import android.app.AlertDialog;
+import android.app.Dialog;
 import android.app.ProgressDialog;
 import android.content.Intent;
 import android.database.Cursor;
@@ -9,21 +10,30 @@ import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.Environment;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.text.Html;
 import android.text.method.LinkMovementMethod;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import java.io.BufferedInputStream;
+import java.io.FileOutputStream;
 import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.URL;
+import java.net.URLConnection;
 
 public class PhotoUploadActivity extends AppCompatActivity implements View.OnClickListener {
 
     private Button buttonChoosePhoto; // 사진 선택 버튼
     private Button buttonUploadPhoto; // 업로드 버튼
+    private Button buttonDownloadPhoto;
     private TextView textView;
     private TextView textViewResponse;
     private ImageView photo_preview_main; //사진 미리보기
@@ -32,6 +42,10 @@ public class PhotoUploadActivity extends AppCompatActivity implements View.OnCli
 
     private  String selectedPath;
 
+    private ProgressDialog pDialog;
+    public static final int progress_bar_type = 0;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -39,6 +53,7 @@ public class PhotoUploadActivity extends AppCompatActivity implements View.OnCli
 
         buttonChoosePhoto = (Button) findViewById(R.id.buttonChoosePhoto);
         buttonUploadPhoto = (Button) findViewById(R.id.buttonUploadPhoto);
+        buttonDownloadPhoto = (Button) findViewById(R.id.buttonDownloadPhoto);
 
         textView = (TextView) findViewById(R.id.textView);
         textViewResponse = (TextView) findViewById(R.id.textViewResponse);
@@ -46,12 +61,14 @@ public class PhotoUploadActivity extends AppCompatActivity implements View.OnCli
 
         buttonChoosePhoto.setOnClickListener(this);
         buttonUploadPhoto.setOnClickListener(this);
+        buttonDownloadPhoto.setOnClickListener(this);
+
     }
 
     private void chooseVideo() {
         Intent intent = new Intent();
-        intent.setType("image/*");
-        intent.setAction(Intent.ACTION_GET_CONTENT);
+        intent.setAction(Intent.ACTION_PICK);
+        intent.setType(android.provider.MediaStore.Images.Media.CONTENT_TYPE);
         startActivityForResult(Intent.createChooser(intent, "Select a Photo "), SELECT_PHOTO);
     }
 
@@ -62,7 +79,8 @@ public class PhotoUploadActivity extends AppCompatActivity implements View.OnCli
                 System.out.println("SELECT_PHOTO");
                 Uri selectedImageUri = data.getData();
                 selectedPath = getPath(selectedImageUri);
-                textView.setText("사진이 선택되었습니다.");
+                Toast toast = Toast.makeText(getApplicationContext(), "사진이 선택되었습니다. \n 사진 업로드를 진행하세요.", Toast.LENGTH_LONG); toast.show();
+
                 try {
                     //선택한 이미지에서 비트맵 생성
                     InputStream inputStream = getContentResolver().openInputStream(data.getData());
@@ -138,6 +156,103 @@ public class PhotoUploadActivity extends AppCompatActivity implements View.OnCli
         }
     }
 
+    private void downloadPhoto() {
+
+        new DownloadFileFromURL().execute("http://52.79.176.116/outputs/final.jpg");
+
+    }
+
+    @Override
+    protected Dialog onCreateDialog(int id) {
+        switch (id) {
+            case progress_bar_type:
+                pDialog = new ProgressDialog(this);
+                pDialog.setMessage("Downloading file. Please wait...");
+                pDialog.setIndeterminate(false);
+                pDialog.setMax(100);
+                pDialog.setProgressStyle(ProgressDialog.STYLE_HORIZONTAL);
+                pDialog.setCancelable(true);
+                pDialog.show();
+                return pDialog;
+            default:
+                return null;
+        }
+    }
+
+    class DownloadFileFromURL extends AsyncTask<String, String, String> {
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            showDialog(progress_bar_type);
+        }
+
+        @Override
+        protected String doInBackground(String... f_url) {
+            int count;
+            try {
+                URL url = new URL(f_url[0]);
+                URLConnection connection = url.openConnection();
+                connection.connect();
+
+                // this will be useful so that you can show a tipical 0-100%
+                // progress bar
+                int lengthOfFile = connection.getContentLength();
+
+                // download the file
+                InputStream input = new BufferedInputStream(url.openStream(),
+                        8192);
+
+                // Output stream
+                OutputStream output = new FileOutputStream(Environment
+                        .getExternalStorageDirectory().toString()
+                        + "/444.jpg");
+
+                byte data[] = new byte[1024];
+
+                long total = 0;
+
+                while ((count = input.read(data)) != -1) {
+                    total += count;
+                    publishProgress("" + (int) ((total * 100) / lengthOfFile));
+
+                    output.write(data, 0, count);
+                }
+
+                // flushing output
+                output.flush();
+
+                // closing streams
+                output.close();
+                input.close();
+
+
+            } catch (Exception e) {
+                Log.e("Error: ", e.getMessage());
+            }
+
+            return null;
+        }
+
+        /**
+         * Updating progress bar
+         * */
+        protected void onProgressUpdate(String... progress) {
+            // setting progress percentage
+            pDialog.setProgress(Integer.parseInt(progress[0]));
+        }
+
+        /**
+         * After completing background task Dismiss the progress dialog
+         * **/
+        @Override
+        protected void onPostExecute(String file_url) {
+            // dismiss the dialog after the file was downloaded
+            dismissDialog(progress_bar_type);
+
+        }
+    }
+
 
     @Override
     public void onClick(View v) {
@@ -147,6 +262,11 @@ public class PhotoUploadActivity extends AppCompatActivity implements View.OnCli
 
         if (v == buttonUploadPhoto) {
             uploadPhoto();
+        }
+
+        if(v == buttonDownloadPhoto) {
+            downloadPhoto();
+
         }
 
     }
